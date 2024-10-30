6f5500ce7774dd47780767a048665cc0c2d4b3a7
Emanuel <86531750+BelmontT@users.noreply.github.com>
2024-10-30 18:57:26 -0300

    Add files via upload

---
+++ pages/plans.php
@@ -0,0 +1,56 @@
+<html lang="pt-BR">
+    <head>
+        <meta charSet="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Find your personal trainer and improve your training to achieve the shape you've always dreamed of.">
+        <script type="text/javascript" src="../layout/javascript/function.js"></script>
+        <link rel="stylesheet" type="text/css" href="../layout/css/style.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+        <link rel="icon" type="image/x-icon" href="../favicon.ico">
+        <title>Studio Vegas</title>
+    </head>
+    <header>
+        <div class="header-content">
+            <a href="../index.html"><img src="../layout/images/logo.png" title="Studio Vegas" class="image-logo"></a>
+            <div class="profile-link profile-button">
+                <i class="bi-person-circle"></i>
+                <div class="profile-link-content">
+                    <a href="login.php"><i class="bi-person-lines-fill"></i> Entrar</a>
+                    <a href="register.php"><i class="bi-person-plus-fill"></i> Registrar</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <body>
+        <div class="contentDiv">
+            <button onclick="history.back()" class="back"><i class="bi-reply" title="Voltar"></i></button>
+            <a href="logout.php" class="logout"><i class="bi-power" title="Sair"></i></a>
+            <div class="container">
+                <h1>
+                    <div>PILATES</div>  
+                    <div>R$ 300,00 ou 10 x 32,00</div>        
+                </h1>
+                <button onclick="window.location.href='https://www.pilatesanytime.com/'">PILATES</button>
+            </div><br>
+
+            <div class="container">
+                <h1>
+                    <div>HIDROGINASTICA</div>
+                    <div>R$ 280 OU 10 X 30,00</div>
+                </h1>
+                <button onclick="window.location.href='https://blog.speedo.com.br/'">HIDROGINASTICA</button>
+            </div><br>
+
+            <div class="container">
+                <h1>
+                    <div>MUSCULAÇÃO</div>
+                    <div>200,00 OU 10 X 22,00</div>
+                </h1>
+                <button onclick="window.location.href='https://vitat.com.br/musculacao/'">MUSCULAÇÃO</button>
+            </div><br>
+        </div>
+    </body>
+    <div class="footer">
+        © Todos os direitos reservados.
+    </div>
+</html>
